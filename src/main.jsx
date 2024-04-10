3941cd95fe6fc2622d109444ea35563e41476d09
Edinson Rivera <edirju@gmail.com>
2024-04-10 16:07:49 -0500

    Cargar todo el repositorio

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscaMovies } from './BuscaMovies'
+import './styles/buscador.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscaMovies />
+  </React.StrictMode>,
+)
